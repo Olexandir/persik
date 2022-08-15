ab3c998841bd34df9d00cb47f961b4bcd76768f5
Alex <alex@iMac-Olex.local>
2022-08-15 21:31:38 +0300

    init: init category card component

---
+++ src/app/components/channel-category-card/channel-category-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-channel-category',
+  templateUrl: 'channel-category-card.component.html',
+  styleUrls: ['channel-category-card.component.scss']
+})
+export class ChannelCategoryComponent {}
